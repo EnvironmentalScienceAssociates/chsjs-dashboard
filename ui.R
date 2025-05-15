7e7fbd2dd7300d08fed2fe2212fadfef15c635b8
Travis Hinkelman <thinkelman@esassoc.com>
2025-05-14 19:46:13 -0700

    Add placeholders for other tabs

--- ui.R
+++ ui.R
@@ -14,22 +14,34 @@ page_navbar(
       includeMarkdown("static/background.md"),
     )
   ),
-  nav_panel(
-    title = "R Markdown Example",
-    layout_columns(
-      col_widths = breakpoints(
-        sm = c(-1, 10, -1),
-        md = c(-2, 8, -2),
-        lg = c(-3, 6, -3)
-      ),
-      HTML(file.path("static", "Example.Rmd") |>
-             knitr::knit(quiet = TRUE) |> 
-             markdown::markdownToHTML(fragment.only = TRUE))
-    )
-  ),
+  # nav_panel(
+  #   title = "R Markdown Example",
+  #   layout_columns(
+  #     col_widths = breakpoints(
+  #       sm = c(-1, 10, -1),
+  #       md = c(-2, 8, -2),
+  #       lg = c(-3, 6, -3)
+  #     ),
+  #     HTML(file.path("static", "Example.Rmd") |>
+  #            knitr::knit(quiet = TRUE) |> 
+  #            markdown::markdownToHTML(fragment.only = TRUE))
+  #   )
+  # ),
   nav_panel(
     title = "Water Quality",
     waterQualityUI("wq")
+  ),
+  nav_panel(
+    title = "Isotopes"
+  ),
+  nav_panel(
+    title = "Groundwater"
+  ),
+  nav_panel(
+    title = "Seagrass"
+  ),
+  nav_panel(
+    title = "Events"
   ),
   nav_spacer(),
   nav_item(a("Source Code", href = "https://github.com/EnvironmentalScienceAssociates/chsjs-dashboard",
