a030fdbe23ef756b90138aecb549563366a0a882
Travis Hinkelman <thinkelman@esassoc.com>
2025-05-15 22:03:37 -0700

    Fix typo

--- R/isotopesUI.R
+++ R/isotopesUI.R
@@ -3,6 +3,6 @@ isotopesUI <- function(id){
   ns = NS(id)
   tagList(
     plotlyOutput(ns("plot")),
-    helpText("Note: The bounding boxes represent potential sources of nitrate (Canion et al., 2020).")
+    helpText("Note: The bounding boxes represent potential sources of nitrate (Canion et al. 2020).")
   )
 }
